d67fe9d696a69202d82ce11da5663f331071536c
Adam Sahuo <adamsahuo@outlook.com>
2024-10-31 23:17:40 +0300

    Add react router dom and create a screens folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Layout,
+  Register,
+  Login,
+  DashboardLayout,
+  Landing,
+  Error,
+} from "./screens/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <Error />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 export default App;
---
+++ frontend/src/screens/index.js
@@ -0,0 +1,6 @@
+export { default as Layout } from "./Layout.jsx";
+export { default as Register } from "./Register.jsx";
+export { default as Login } from "./Login.jsx";
+export { default as DashboardLayout } from "./DashboardLayout.jsx";
+export { default as Landing } from "./Landing.jsx";
+export { default as Error } from "./Error.jsx";
